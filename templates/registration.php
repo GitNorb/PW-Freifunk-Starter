b8e9578e01b7e68361ea647a88960bb5a6a70a3b
kreativmonkey <kreativmonkey@freifunk-myk.de>
2016-02-03 01:08:41 +0100

    Authorized

--- templates/registration.php
+++ templates/registration.php
@@ -24,10 +24,11 @@ if($input->post->submit) {
             $new_user->firstname = $nuser["firstname"];
             $new_user->lastname = $nuser["lastname"];
             $new_user->admin_theme = "AdminThemeReno";
+            $new_user->authkey = base64_encode($nuser["username"].$today);
             $new_user->save();
             $new_user->of(true);
 
-            $content .= "<div data-alert class='alert-box success radius'>Die Registrierung war erfolgreich</div>";
+            $content .= "<div data-alert class='alert-box success radius'>Die Registrierung war erfolgreich, du erhälst nun eine E-Mail mit einem Aktivierungslink.</div>";
           } else {
             $content .= "<div data-alert class='alert-box alert'>
             Das Anlegen eines LDAP Users hat leider nicht funktioniert. Bitte versuche es zu einem späteren Zeitpunkt noch einmal.
@@ -38,7 +39,21 @@ if($input->post->submit) {
                         Der Benutzername <strong>{$nuser['username']}</strong> ist schon vergeben!
                       </div>";
         }
+} elseif($input->get->authorize) {
+  $auth_user = $users->get("authorize={$input->get->authorize}");
+  if(!$auth_user instanceof NullPage){
+    if($auth_user->auth == true){
+      $contant .="<div data-alert class='alert-box alert'>
+        Dein Account wurde bereits Aktiviert.
+      </div>";
+    } else {
+      $auth_user->of(false);
+      $auth_user->auth = true;
+      $auth_user->save();
+      $auth_user->of(true);
+    }
   }
+}
 
 
 if(!wire('user')->isLoggedin() && !wire('input')->post->submit){
