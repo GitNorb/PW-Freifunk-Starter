90ac0e14bb923d393cd7acaaf00fe353390e7fbd
kreativmonkey <kreativmonkey@freifunk-myk.de>
2016-06-30 22:25:28 +0200

    Change Blog render

--- templates/list_blog.php
+++ templates/list_blog.php
@@ -1,37 +1,25 @@
 <?php
 
-$output = '';
+$output_posts = '';
 
 $posts = $pages->find("template=post,sort=-date");
 
 foreach($posts as $post){
-  $image = (count($post->images) ? "<img class='responsive' src='{$post->images->first()->size(300,200)->url}'></img>" : "");
 
-  $output .= "<article id='article-{$post->id}' class='row article'>
-                <div class='medium-3 columns'>
-                  $image
-                </div>
-                <div class='medium-9 columns'>
-                  <h4 class='tobheader'><small>". strftime('%d %b %Y', $post->getUnformatted('date')) ."</small></h4>
-                  <a href='{$post->httpUrl}'><h3>{$post->title}</h3></a>
-                  <p>
-                      {$post->get('body|summary')}
-                  </p>
-                </div>
-            </article><!-- #article-7 -->
-            <hr>";
+
+  $template = new TemplateFile($config->paths->templates . "markup/post_small.inc");
+  $template->set('title', $post->title);
+  $template->set('date', $post->getUnformatted('date'));
+  $template->set('url', $post->httpUrl);
+  $template->set('image', (count($post->images) ? "<img class='responsive' src='{$post->images->first()->size(300,200)->url}'></img>" : ""));
+  $template->set('body', $post->get('summary|body'));
+  $template->set('id', $post->id);
+
+  $output_posts .= $template->render();
+
 };
 
-$content = "<div id='article' class='blog-list large-9 columns'>
-              $output
-            </div><!-- #article -->
-            <div id='sidebar' class='large-3 columns'>
-              <h5>Neueste Router</h5>
-              <ul>
-                <li>Koblenz, Marktplatz</li>
-                <li>Koblenz, Marktplatz</li>
-                <li>Koblenz, Marktplatz</li>
-                <li>Koblenz, Marktplatz</li>
-                <li>Koblenz, Marktplatz</li>
-              </ul>
-            </div>";
+$template = new TemplateFile($config->paths->templates . "markup/list_blog.inc");
+$template-> set('posts', $output_posts);
+
+$content = $template->render();
