eec6037bdde37af13129ba7288e467eb2e2ad7d0
kreativmonkey <kreativmonkey@freifunk-myk.de>
2016-07-03 17:07:12 +0200

    add Link and change logic

--- templates/router.php
+++ templates/router.php
@@ -1,48 +1,43 @@
 <?php
 
 $companyRouter = strtolower($page->parent->title) ."-". strtolower($page->title);
-$page->set('stable', json_decode(file_get_contents("http://firmware.freifunk-myk.de/.static/filter/?filter={$companyRouter}&branch[]=stable&output=json")));
-$page->set('beta', json_decode(file_get_contents("http://firmware.freifunk-myk.de/.static/filter/?filter=$companyRouter&branch[]=beta&output=json")));
+$page->set('firmwareList', json_decode(file_get_contents("http://firmware.freifunk-myk.de/.static/filter/?filter={$companyRouter}&branch[]=stable&branch[]=beta&output=json")));
 
 
 $stableSysupgrade = "";
 $stableFactory = "";
 $betaFactory = "";
 $betaSysupgrade = "";
-foreach ($page->stable as $firmware) {
-  $template = new TemplateFile($config->paths->templates . "markup/table_router_firmware.inc");
+foreach ($page->firmwareList as $key => $firmware) {
+  $template = new TemplateFile($config->paths->templates . "markup/router_firmware_table_tr.inc");
   $template->set('version', $firmware->version);
   $template->set('builddate', $firmware->builddate);
   $template->set('release', $firmware->release);
   $template->set('hash', ($firmware->hash ? $firmware->hash->md5 : ""));
   $template->set('filename', $firmware->filename);
-  $template->set('url', "#");
+  $template->set('url', "http://firmware.freifunk-myk.de". $key);
 
   if($firmware->sysupgrade){
-    $stableSysupgrade .= $template->render();
+    if($firmware->branch == "stable"){
+      $stableSysupgrade .= $template->render();
+    } else if ($firmware->branch == "beta"){
+      $betaSysupgrade .= $template->render();
+    }
   } else {
-    $stableFactory .= $template->render();
-  };
-}
-foreach ($page->beta as $firmware) {
-  $template = new TemplateFile($config->paths->templates . "markup/table_router_firmware.inc");
-  $template->set('version', $firmware->version);
-  $template->set('builddate', $firmware->builddate);
-  $template->set('release', $firmware->release);
-  $template->set('hash', ($firmware->hash ? $firmware->hash->md5 : ""));
-  $template->set('filename', $firmware->filename);
-  $template->set('url', "#");
-
-  if($firmware->sysupgrade){
-    $betaSysupgrade .= $template->render();
-  } else {
-    $betaFactory .= $template->render();
-  };
+    if($firmware->branch == "stable"){
+      $stableFactory .= $template->render();
+    } else if ($firmware->branch == "beta") {
+      $betaFactory .= $template->render();
+    }
+  }
 }
 
-$page->set('stableSysupgrade', $stableSysupgrade);
-$page->set('stableFactory', $stableFactory);
-$page->set('betaSysupgrade', $betaSysupgrade);
-$page->set('betaFactory', $betaSysupgrade);
+$table = new TemplateFile($config->paths->templates . "markup/router_firmware_accordion.inc");
+$table->set('stableSysupgrade', $stableSysupgrade);
+$table->set('stableFactory', $stableFactory);
+$table->set('betaSysupgrade', $betaSysupgrade);
+$table->set('betaFactory', $betaSysupgrade);
+
+$page->set('firmwareTable', $table->render());
 
 $content = renderPage();
