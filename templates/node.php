b30990706b0f54807a2b22bccd49e4c8ed215ceb
Kreativmoneky <sebastian.preisner@gmail.com>
2016-07-12 01:06:14 +0200

    Umkreissuche für Nodes

--- templates/node.php
+++ templates/node.php
@@ -2,23 +2,37 @@
 $config->styles->add($config->urls->templates.'css/leaflet.css');
 $config->scripts->add($config->urls->templates.'js/leaflet-src.js');
 
+$latitude = str_replace(',','.',$page->latitude);
+$longitude = str_replace(',','.',$page->longitude);
+
+$nearnodes = umkreissuche("node", $page->latitude, $page->longitude, 5);
+
+$marker = '';
+foreach($nearnodes as $node){
+  $marker .= "L.circle([".str_replace(',','.',$node->latitude).",".str_replace(',','.',$node->longitude)."], 10,{
+        color:'blue',
+        fillColor:".($page->online == 1 ? "'green'" : "'red'"). "
+      }).addTo(map);";
+}
+
 $script = "<script>
-            var map = L.map('map').setView([{$page->latitude}, {$page->longitude}], 16);
+            var map = L.map('map').setView([{$latitude}, {$longitude}], 16);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
               maxZoom: 19,
               attribution: '&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>'
             }).addTo(map);
 
-            L.circle([{$page->latitude}, {$page->longitude}], 10, {
+            L.circle([$latitude, $longitude], 10, {
               color:".($page->online == 1 ? "'green'" : "'red'").",
               fillColor: ".($page->online == 1 ? "'green'" : "'red'")."
             }).addTo(map);
 
+            $marker
+
             map.invalidateSize();
           </script>";
 
-$nearnodes = umkreissuche("node", "{$page->latitude}", "{$page->longitude}");
 
 $page->losttime = time_elapsed_string($page->getUnformatted('lastseen'));
 $content = renderPage();
